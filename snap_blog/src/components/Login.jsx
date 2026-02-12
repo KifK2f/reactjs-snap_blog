6ba791815424f9db8ecaed8884c0e42bc5bf3ae4
Kifk2f <fianyofriedo2@gmail.com>
2026-02-12 00:15:08 +0000

    Simuler un Token et l'enrégistrer dans le localstorage

--- snap_blog/src/components/Login.jsx
+++ snap_blog/src/components/Login.jsx
@@ -11,17 +11,37 @@ class Login extends React.Component {
     }
 
 
+    // //A chaque fois que je vais changer un truc dans le champ email afiche ce message  dans la console
+    // email = event =>{
+    //     console.log("Changement dans le champ email");
+    // }
+
     //A chaque fois que je vais changer un truc dans le champ email afiche ce message  dans la console
-    email = event =>{
-        console.log("Changement dans le champ email");
+    onEmailChange = event =>{
+        this.setState({email: event.target.value}, () => {
+            console.log(this.state);
+        })
     }
+
+    onPasswordChange = event =>{
+        this.setState({password: event.target.value}, () => {
+            console.log(this.state);
+        })
+    }
+
+    handleFormSubmit = event => {
+        event.preventDefault() // Empêche le comportement par défaut du formulaire (rechargement de la page)
+        localStorage.setItem("token", "Friedo12345") // Simule la création d'un token d'authentification en le stockant dans le localStorage
+    }
+
+
 
     render() {
         return(
             <>
-                <form action="POST">
-                   <input type="text" name='email' placeholder='Votre email' onChange={this.email} />
-                   <input type="password" name='password' placeholder='Votre mot de passe' />
+                <form action="POST" onSubmit={this.handleFormSubmit}>
+                   <input type="text" name='email' placeholder='Votre email' onChange={this.onEmailChange} />
+                   <input type="password" name='password' placeholder='Votre mot de passe' onChange={this.onPasswordChange} />
                    <button type='submit'>Me connecter</button>
                 </form>
             </>
--- snap_blog/src/components/Photos.jsx
+++ snap_blog/src/components/Photos.jsx
@@ -1,18 +1,24 @@
 import axios from 'axios';
 import React from 'react'
+import { Navigate } from 'react-router-dom';
 
 class Photos extends React.Component {
 
     constructor(){
         super()
         this.state = {
-            photos : []
+            photos : [],
+            redirect: false
         }
     }
 
     //Quand le composant est monté, on va faire une requete pour aller chercher les photos
     componentDidMount(){
-        axios.get("http://127.0.0.1:8000/api/photos")
+
+        // vérifier si au niveau du sessionStorage on a un token d'authentification(simulation d'une authentification)
+        // if(sessionStorage.getItem("token")){
+        if(localStorage.getItem("token")){
+            axios.get("http://127.0.0.1:8000/api/photos")
         .then(res => {
             // console.log(res);
             this.setState({photos: res.data}, () => {
@@ -21,9 +27,20 @@ class Photos extends React.Component {
         .catch(error => {
             console.log(error.response);
         }) // Si la promesse est rejetée, on affiche l'erreur dans la console
+
+        } else {
+            // Sinon, on redirige vers la page de login
+            this.setState({redirect: true})
+        }
+
+        
     }
 
     render(){
+        if(this.state.redirect === true){ // Si égal à true, on redirige vers la page de login
+            return ( <Navigate to="/login" />)
+        }
+
         return(
             <>
                 {/* Sans key :
